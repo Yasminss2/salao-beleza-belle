7039dc92831d016cfae606202ef42649a6e44596
Yasmin Santos Sousa <yasminssousousa@gmail.com>
2026-05-29 14:50:43 -0300

    Update dados.js

--- Projeto-Salão-de-Beleza/js/dados.js
+++ Projeto-Salão-de-Beleza/js/dados.js
@@ -5,7 +5,7 @@ const ITENS = [
   { nome: "Penteados",      descricao: "Estilos e penteados profissionais e Personalizados para qulaquer ocasião", preco: "R$ 149,99", imagem: "img/penteado.png" },
   { nome: "Escovação", descricao: "Escova de alta qualidade para dar volume e brilho ao seu cabelo",      preco: "R$ 89,99", imagem: "img/escovacao.png" },
   { nome: "Progressiva", descricao: "Tratamento capilar que alinha o cabelo, reduz o frizz e proporcioana brilho e saúde", preco: "R$ 110,00", imagem: "img/progressiva.png" },
-  { nome: "Manicure", descricao: "Corte, Lixamento, cuidado com cúticulas e esmaltação impecável",      preco: "R$ 89,99", imagem: "../img/manicure.png" },
+  { nome: "Manicure", descricao: "Corte, Lixamento, cuidado com cúticulas e esmaltação impecável",      preco: "R$ 89,99", imagem: "img/manicure.png" },
   { nome: "Pedicure", descricao: "Cuidado completo para seus pés com hidratação, remoção e rachaduras, massagem relaxante e esmaltação",      preco: "R$ 79,99", imagem: "img/pe.png" },
   { nome: "Maquiagem", descricao: "Realce sua maquiagem sofisticada com prepração de pele e produção completa, fique preprada em qualquer ocasião especial",      preco: "R$ 120,00", imagem: "img/maquiagem.png"}
 ];
